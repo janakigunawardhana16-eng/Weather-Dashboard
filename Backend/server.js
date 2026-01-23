b1cab18bd1cf5538e2d59236ae56e351a2afb4af
Chirani-Dewnara <chiranidewnara424@gmail.com>
2026-01-23 15:43:41 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const weatherRoutes = require("./routes/weather");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/weather", weatherRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
